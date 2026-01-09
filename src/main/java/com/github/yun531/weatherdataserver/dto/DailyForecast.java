fd701cad62c3e9b2d05f04311e1db1b4a55b336a
GSpy <55821668+G5py@users.noreply.github.com>
2026-01-09 15:28:05 +0900

    fix : api 응답 클래스의 접근 제한자 때문에 응답이 비어있는 문제 해결.

--- src/main/java/com/github/yun531/weatherdataserver/dto/DailyForecast.java
+++ src/main/java/com/github/yun531/weatherdataserver/dto/DailyForecast.java
@@ -1,9 +1,11 @@
 package com.github.yun531.weatherdataserver.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 @AllArgsConstructor
 public class DailyForecast {
     String regionCode;
